8deb1f53d4709fc7f6c145073339db3977dda8f3
Astating <79377061+Astating@users.noreply.github.com>
2022-09-19 19:17:23 +0200

    Create docker-contained postgreSQL database (#11)
    * Create docker-contained postgreSQL database
    
    * remove __pycache__ and add it to the .gitignore list
    
    Co-authored-by: Constant G <constant.guilbert@polyconseil.fr>

--- Jardipotes/wsgi.py
+++ Jardipotes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for JardiPotes project.
+WSGI config for Jardipotes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JardiPotes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jardipotes.settings')
 
 application = get_wsgi_application()
