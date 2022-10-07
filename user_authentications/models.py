13f9360882ea27f933a8b34f6ed83b22e471549b
Quentin <91956889+SadeQL@users.noreply.github.com>
2022-10-07 14:48:20 +0200

    fix: remove default fields for username, email, and password

--- user_authentications/models.py
+++ user_authentications/models.py
@@ -6,12 +6,12 @@ from datetime import datetime
 class User(models.Model):
 
     username = models.CharField(
-        max_length=25, blank=False, unique=True, null=False, default="test_user")
+        max_length=25, blank=False, unique=True, null=False)
     first_name = models.CharField(max_length=25, blank=False, null=True)
     last_name = models.CharField(max_length=25, blank=False, null=True)
     email = models.EmailField(
-        max_length=25, blank=False, unique=True, default="test@test.test")
-    password = models.CharField(max_length=25, blank=False, default="testtest")
+        max_length=25, blank=False, unique=True)
+    password = models.CharField(max_length=25, blank=False)
     password_updated_at = models.DateTimeField(default=now, null=True)
     profile_image = models.URLField(null=True)
     bio = models.TextField(blank=True, null=True)
