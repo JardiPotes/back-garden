c6b0f591a71ff781a05f3851c4e634b0874f57d8
Quentin <91956889+SadeQL@users.noreply.github.com>
2022-11-04 12:07:30 +0100

    feat: added superuser creation & password reset endpoints (#31)
    * feat: added superuser creation & password reset endpoints
    
    * Update accounts/views.py
    
    Co-authored-by: Astating <79377061+Astating@users.noreply.github.com>
    
    Co-authored-by: Astating <79377061+Astating@users.noreply.github.com>

--- Jardipotes/urls.py
+++ Jardipotes/urls.py
@@ -4,8 +4,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+""" 
+POST ${API_URL}/ - request a reset password token by using the email parameter
+POST ${API_URL}/confirm/ - using a valid token, the users password is set to the provided password
+POST ${API_URL}/validate_token/ - will return a 200 if a given token is valid
+"""
+
 urlpatterns = [
     re_path('admin/', admin.site.urls),
     re_path('api/users/', include('accounts.urls')),
+
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
 import json
 
 """Create a user
@@ -71,6 +76,56 @@ def update_user(request, pk):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+
+""" password reset method """
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    """
+    Handles password reset tokens
+    When a token is created, an e-mail needs to be sent to the user
+    :param sender: View Class that sent the signal
+    :param instance: View Instance that sent the signal
+    :param reset_password_token: Token Model Object
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    # send an e-mail to the user
+    # reset password confirmation url looks like this: host/api/users/password_reset/?token=TOKEN
+    context = {
+        'current_user': reset_password_token.user,
+        'first_name': reset_password_token.user.first_name,
+        'email': reset_password_token.user.email,
+        'reset_password_url': "{}?token={}".format(
+            instance.request.build_absolute_uri(
+                reverse('password_reset:reset-password-confirm')),
+            reset_password_token.key)
+    }
+
+    # render email text
+    email_html_message = render_to_string(
+        'user_reset_password.html', context)
+    email_plaintext_message = render_to_string(
+        'user_reset_password.txt', context)
+
+    msg = EmailMultiAlternatives(
+        # title:
+        "Password Reset for {title}".format(
+            title="Réinitialiser le code secret pour JardiPotes"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "sadefryt@gmail.com",
+        # to:
+        [reset_password_token.user.email]
+    )
+    msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
 
 
 @api_view(['DELETE'])
