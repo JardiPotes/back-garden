f90fbe5e40c3ac6bffb2052318cdfd7a267a87b8
Constant G <constant.guilbert@polyconseil.fr>
2022-12-13 16:36:45 +0100

    Differentiate factories create_dict from create

--- accounts/factories/garden.py
+++ accounts/factories/garden.py
@@ -1,5 +1,8 @@
+from accounts.models import Garden
+
+
 class GardenFactory:
-    def create_garden(
+    def create_garden_dict(
         userId: str = "1",
         title: str = "ma plantation",
         description: str = "un joli jardin",
@@ -7,3 +10,6 @@ class GardenFactory:
         zipcode: str = "75000",
     ):
         return locals()
+
+    def create_garden(**kwargs):
+        return Garden.objects.create(**GardenFactory.create_garden_dict(**kwargs))
