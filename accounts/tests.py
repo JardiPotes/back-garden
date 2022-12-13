c0bd237572619d5c2d045835e63dcec8e764e76e
Victoire Stahl <91953020+Vico4@users.noreply.github.com>
2022-12-13 15:10:16 +0100

    garden factory and test (#38)

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from accounts.factories.user import UserFactory
-from .models import User
+from accounts.factories.garden import GardenFactory
+from .models import User, Garden
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
@@ -19,3 +20,18 @@ class CreateUserTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().email, 'helloworld@test.test')
+
+
+class CreateGardenTests(APITestCase):
+    def test_create_garden(self):
+
+        url = reverse('create_garden')
+        factoryUser = UserFactory.create_user()
+        user = User.objects.create(**factoryUser)
+        print(user)
+        data = GardenFactory.create_garden(userId=user.id, title='au vert')
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Garden.objects.count(), 1)
+        self.assertNotEqual(Garden.objects.get().title, '')
